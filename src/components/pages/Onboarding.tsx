b53b11386a569fdd057a82513abe66b4342dc0d9
Luca Milanese <luca.milanese90@gmail.com>
2022-08-24 10:05:27 +0200

    feat: update asset details page

---
+++ src/components/pages/Onboarding.tsx
@@ -0,0 +1,6 @@
+import React, { ReactElement } from 'react'
+import OnboardingSection from './Home/Onboarding'
+
+export default function AboutPage(): ReactElement {
+  return <OnboardingSection />
+}
