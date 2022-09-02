0ab038c41307a4bba97d0eeca59a5a2f361a43c6
Luca Milanese <luca.milanese90@gmail.com>
2022-09-02 17:17:58 +0200

    refactor: decrease apge header padding

--- src/components/templates/Page.tsx
+++ src/components/templates/Page.tsx
@@ -68,6 +68,7 @@ export default function Page({
           description={description}
           center={headerCenter}
           powered={isHome}
+          isHome={isHome}
         />
       )}
       {isSearch && (
