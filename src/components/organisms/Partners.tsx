be04440d79886acf0f6bf1167f008a1a9a69e5c2
Luca Milanese <luca.milanese90@gmail.com>
2022-10-28 11:04:10 +0200

    enhancement: resize partner logos

--- src/components/organisms/Partners.tsx
+++ src/components/organisms/Partners.tsx
@@ -1,18 +1,52 @@
 import React, { ReactElement } from 'react'
 import styles from './Partners.module.css'
 import classNames from 'classnames/bind'
-import { ReactComponent as RWLogo } from '../../images/RW-logo.svg'
-import { ReactComponent as GaiaXNetherlandsLogo } from '../../images/gaia-x-netherlands-logo.svg'
-import { ReactComponent as DBCLogo } from '../../images/DBC-logo.svg'
-import { ReactComponent as DeltaDaoLogo } from '../../images/deltaDAO_Logo_Hoch_RGB_positiv.svg'
+import { graphql, useStaticQuery } from 'gatsby'
 
 const cx = classNames.bind(styles)
+
+const homePageHeaderQuery = graphql`
+  query PartnersQuery {
+    partners: allFile(
+      filter: { absolutePath: { regex: "/src/images/partners/" } }
+      sort: { fields: [base] }
+    ) {
+      edges {
+        node {
+          childImageSharp {
+            id
+            original {
+              src
+            }
+          }
+        }
+      }
+    }
+  }
+`
+interface Partners {
+  partners: {
+    edges: {
+      node: {
+        childImageSharp: {
+          id: string
+          original: {
+            src: string
+          }
+        }
+      }
+    }[]
+  }
+}
 
 export default function Partners({
   className
 }: {
   className?: string
 }): ReactElement {
+  const data: Partners = useStaticQuery(homePageHeaderQuery)
+  const { partners } = data
+
   return (
     <div
       className={cx({
@@ -20,10 +54,13 @@ export default function Partners({
         [className]: className
       })}
     >
-      <RWLogo />
-      <DBCLogo />
-      <GaiaXNetherlandsLogo />
-      <DeltaDaoLogo />
+      {partners.edges.map((e) => (
+        <img
+          key={e.node.childImageSharp.id}
+          className={styles.logo}
+          src={e.node.childImageSharp.original.src}
+        />
+      ))}
     </div>
   )
 }
